9544c27bbf0783b7a8a7c166679b4b56acbaa0b7
Pairman <pairmanxlr@gmail.com>
2026-05-20 11:06:39 +0800

    update configs

--- signer.go
+++ signer.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"crypto/ed25519"
-	"crypto/rand"
 	"encoding/base64"
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	pub, priv, _ := ed25519.GenerateKey(rand.Reader)
-	fmt.Println("Private key:", base64.StdEncoding.EncodeToString(priv))
-	fmt.Println("Public key:", base64.StdEncoding.EncodeToString(pub))
-	data, _ := os.ReadFile("config.json")
-	sig := ed25519.Sign(ed25519.PrivateKey(priv), data)
-	fmt.Println("Signature:", base64.StdEncoding.EncodeToString(sig))
+	flag.Parse()
+
+	privKey, err := base64.StdEncoding.DecodeString(os.Getenv("TELEMETRY_PRI_KEY"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	configBytes, err := os.ReadFile("config.json")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	err = os.WriteFile("config.json.sig", []byte(
+		base64.StdEncoding.EncodeToString(
+			ed25519.Sign(ed25519.PrivateKey(privKey), configBytes))), 0644)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
